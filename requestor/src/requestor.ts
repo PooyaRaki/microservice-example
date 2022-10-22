1cfa712124ffb34a673df56956425034c9a53e10
PooyaRaki <pooya.raki@gmail.com>
2022-10-22 21:28:29 +0330

    fix => Added some todo notes

--- requestor/src/requestor.ts
+++ requestor/src/requestor.ts
@@ -3,6 +3,9 @@ import { ProduceMessage } from 'producer';
 
 const bootstrap = async () => {
     await ProduceMessage('Hi,');
+    // @todo fetch token to check it later in fetch message
+    // @todo here we can consume another `message.v1.registered` event to fetch the token
+    // @todo then we check it when we are polling messages
     const message = await FetchMessage();
 
     console.log(message);
