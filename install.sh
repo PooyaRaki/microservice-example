1532e10f6fac814d61daa1e2dc9f2175cf9fa61d
PooyaRaki <pooya.raki@gmail.com>
2022-10-23 23:19:02 +0330

    feature => ENV files are commited. fix => All the yml files has been merged into one single file

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ./build
 docker network inspect microservice_example >/dev/null 2>&1 || \
     docker network create --driver bridge microservice_example
 
-docker compose -f database.yml -f broker.yml -f services.yml up -d
+docker compose -f services.yml up -d
 docker exec build-broker-service-1 npm run migration:run
 
 cd ..
