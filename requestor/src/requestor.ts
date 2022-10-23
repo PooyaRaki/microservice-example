5c3c48c93ca36570ab09331fc915862538b15e82
PooyaRaki <pooya.raki@gmail.com>
2022-10-23 09:11:49 +0330

    fix => Added return type to Requestor bootstrap

--- requestor/src/requestor.ts
+++ requestor/src/requestor.ts
@@ -1,9 +1,9 @@
 import { FetchMessage } from 'get';
 import { ProduceMessage } from 'producer';
 
-const bootstrap = async () => {
+const bootstrap = async (): Promise<void> => {
     await ProduceMessage('Hi,');
-    // @todo fetch token to check it later in fetch message
+    // @todo fetch token to check it later in FetchMessage
     // @todo here we can consume another `message.v1.registered` event to fetch the token
     // @todo then we check it when we are polling messages
     const message = await FetchMessage();
